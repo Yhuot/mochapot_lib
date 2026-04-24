cdeb2156905dbe96c04e940dbce5889f1ac185ce
Yhuot <alexandre.macena.ok@gmail.com>
2026-04-24 14:47:45 -0300

    MAJOR changes to the locks, yes, lockS, on plural. fuck

--- src/concurrency/locks/mocha_lock_new.rs
+++ src/concurrency/locks/mocha_lock_new.rs
@@ -16,10 +16,11 @@ reader_state -> 0..n
     if reader_state is 1, there is a writer;
     if reader_state is > 1, aka n, there are n-1 readers;
 
-writer_state -> 0..n
+writer_state -> -1..n
 
     if writer_state is 0, no queued writer access;
     if writer_state is > 0, aka n, there are n queued writers.
+    if writer_state is -1, this is an unlikely but possible case, though priority is for writers, this was the only way to avoid a clusterfuck level fuckass lowkeynuenely annoying problem.
 
     the way this works is, upon attempting to acquire writer access, a writer will flip the reader_state, making it so readers cannot be acquired.
     then, it will add to writer_state with a fetch_add(1, AcqRel), whatever that function returns, is added to 1, re result of that
@@ -32,26 +33,9 @@ writer_state -> 0..n
 struct MochaLockLock {
     reader_state: AtomicUsize,
     writer_state: AtomicIsize,
-    lock_of_importance: AtomicI32 // was gonna use bool, but it's the same space, processor likely doesn't care.
 }
 
 impl MochaLockLock {
-
-    fn important<F, T>(&self, operation: F) -> ImportantResult<'_, T> 
-        where F: Fn() -> T
-    {
-        loop {
-            match self.lock_of_importance.compare_exchange(0, 1, Ordering::AcqRel, Ordering::Acquire) {
-                Ok(_) => {
-                    let res = ImportantResult{lock: &self.lock_of_importance, value: operation()};
-                    self.lock_of_importance.store(0, Ordering::Release);
-                    wake_all_by_memory(self.lock_of_importance.as_ptr() as *const i32);
-                    return res
-                },
-                Err(state) => wait_for_memory(self.lock_of_importance.as_ptr() as *const i32, state),
-            }
-        }
-    }
 
     // can only be called from reader_state > 1.
     fn free_reader(&self) {
@@ -66,18 +50,45 @@ impl MochaLockLock {
     // can be called from ANY state.
     fn request_read(&self) {
         loop {
-            match self.writer_state.fetch_update(Ordering::AcqRel, Ordering::Acquire, 
-                |ws| {
-                    if ws == 0 {
-                        // if writer_state is 0, that's our greenlight right there.
-                        Some(0)
-                    } else {
-                        None
+            match self.writer_state.compare_exchange(0, -1, Ordering::AcqRel, Ordering::Acquire) {
+                Ok(_) => {
+                    // if the lock was, in fact, free of writer intent
+                    // we add the reader to the pile:
+                    let res = self.reader_state.fetch_update(Ordering::AcqRel, Ordering::Acquire,
+                        |state|{
+                            if state == 0 {
+                                // if 0, return 2, meaning first reader!
+                                Some(2)
+                            } else if state == 1 {
+                                // shouldn't be possible, but still, i might be insane.
+                                None
+                            } else {
+                                // if > 1, increase reader count :D
+                                Some(state+1)
+                            }
+                        }
+                    );
+                    // we then set the writer state back to what we found:
+                    self.writer_state.store(0, Ordering::Release);
+                    // and also wake all mfs that were waiting on that -1 we set before.
+                    wake_all_by_memory(self.writer_state.as_ptr() as *const i32);
+                    match res {
+                        Ok(_) => {
+                            // if code got to this point, reader suceeded.
+                            // we can then return and allow the reader to be acquired
+                            return
+                        },
+                        Err(_) => {
+                            // again, shouldn't be possible, but if the cpu get's whacky or some random cosmic ray decided to flip a bit or idk, maybe the universe itself wanted this to happen:
+                            // we continue the loop to try again.
+                            continue
+                        },
                     }
-                }
-            ) {
-                Ok(_) => todo!(),
-                Err(_) => todo!(),
+                },
+                Err(value) => {
+                    // if there IS writer intent, we fookin wait.
+                    wait_for_memory(self.writer_state.as_ptr() as *const i32, value as i32);
+                },
             }
         }
     }
@@ -87,10 +98,8 @@ impl MochaLockLock {
         self.reader_state.store(2, Ordering::Release);
     }
 
-    // can be called from ANY state
-    fn request_write(&self, conversion: bool) {
-        // by doing this we both acquire an ID and signify writer intent.
-        let mut counter = match self.writer_state.fetch_update(
+    fn try_start_write_intent(&self) -> isize {
+        match self.writer_state.fetch_update(
             Ordering::AcqRel, 
             Ordering::Acquire,
             |state| {
@@ -107,13 +116,19 @@ impl MochaLockLock {
             // if
             Ok(count) => count,
             Err(_) => -1,
-        };
+        }
+    }
+
+    // can be called from ANY state
+    fn request_write(&self, conversion: bool) {
+        // by doing this we both acquire an ID and signify writer intent.
+        let mut counter = self.try_start_write_intent();
         if conversion {
             self.free_reader();
         }
         loop {
             if counter == -1 {
-
+                counter = self.try_start_write_intent();
             }
             if counter == 0 {
                 loop {
